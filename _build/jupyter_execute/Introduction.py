a530882945a105061c411385662fd62e68f36f85
StraightDraw <robbsinn@gmail.com>
2021-01-11 13:21:54 -0500

    updating section 1.1

---
+++ _build/jupyter_execute/Introduction.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Linear Algebra Using MATLAB
+
+# ## Course activities and labs for UNG's Linear Algebra (MATH 3650)
+# Created by Robb Sinn for use with the excellent online [Linear Algebra textbook](https://textbooks.math.gatech.edu/ila/index.html) by Dan Margalit and Joseph Rabinoff.
+
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# 
+# 
+# ch1/0
+# ch2/0
+# ```
+# 
