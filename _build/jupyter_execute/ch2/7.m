68b540e52f02241f9b3a3e4f98b1095876b6d709
StraightDraw <robbsinn@gmail.com>
2021-02-25 15:46:30 -0500

    added 2.7 and 2.8

--- _build/jupyter_execute/ch2/7.m
+++ _build/jupyter_execute/ch2/7.m
@@ -1,4 +1,63 @@
 # 2.7 Basis and Dimension
 
-m = randi(4)
+We are in a great position to understand what it means to be a basis and the dimension of subspace because we have been writing solutions to systems of equations in vector form since chpater 1. We repeat this essential definition directly from the Margalit text.
 
+Basis
+: Let $V$ be a subspace of Rn. A *basis* of $V$ is a set of vectors $\{\vec v_1,\vec v_2, \dots , \vec v_m \}\in V$ such that:
+1. $V = \text{Span}\{\vec v_1,\vec v_2, \dots , \vec v_m \}$, and
+2. the set $\{\vec v_1,\vec v_2, \dots , \vec v_m \}$ is linearly independent.
+
+## Finding a Basis
+
+### Example 1: The basis for a set of vectors.
+
+Given the following set of vectors, find a basis for the $\text{Span}\{\vec v_1,\vec v_2,\vec v_3 \}$.
+
+$$\vec v_1 =\left[\begin{array}{r}-1\\-4\\1\\\end{array}\right],\vec v_2=\left[\begin{array}{r}-6\\-20\\6\\\end{array}\right],\vec v_3=\left[\begin{array}{r}-4\\-13\\4\\\end{array}\right]$$
+
+Let's gather the column vectors into a matrix $M$ for convenience.
+
+M = [-1 -6 -4 ; -4 -20 -13 ; 1 6 4 ];
+v1 = M(:,1)
+v2 = M(:,2)
+v3 = M(:,3)
+
+To find the basis for a $\text{Span}$ of a set of vectors, we need to know how many of the vectors are linearly independent. Let's row reduce $M$.
+
+rref(M)
+
+From the RREF of $M$, we can determine the linear dependence relation which indicates 
+
+$$\vec v_3 = \frac{1}{2}\vec v_1 - \frac{3}{4}\vec v_2$$
+
+and the $\text{Span}$ for these vectors is a two dimensional subspace. This means that the vectors $\vec v_1$ and $\vec v_2$ form a basis for the $\text{Span}$.
+
+$$\mathcal B = \left\{ \left[\begin{array}{r}-1\\-4\\1\\\end{array}\right], \left[\begin{array}{r}-6\\-20\\6\\\end{array}\right] \right\}$$
+
+As the Margalit text points out, there are infinitely many bases for this subspace, but the $\text{Span}$ of each of these will be identical.
+
+### Example 2: Another basis for a set of vectors.
+
+Given the following set of vectors shown as columns of the matrix $R$, find a basis for the $\text{Span}\{\vec v_1,\vec v_2,\vec v_3, \vec v_4, \vec v_5 \}$.
+
+R = [8 -4 -4 0 11 ; -2 5 3 4 3 ; 6 -3 -3 0 7 ; 0 -4 -2 -4 -5 ]
+
+Since the vectors live in $\mathbb R^4$, we can have at most four linearly independent vectors and, correspondingly, 
+a 4-dimensional subspace.
+
+rref(R)
+
+Upon row reducing, we find three pivots, so we know that vectors $\vec v_3,\vec v_4$ can be written as linear combinations of the other three. The vectors that correpond to the pivot columns are linearly independent and form a basis for the $\text{Span}$ of this set of vectors.
+
+$$\mathcal B = \left\{  \left[\begin{array}{r}8\\-2\\6\\0\\\end{array}\right], \left[\begin{array}{r}-4\\5\\-3\\-4\\\end{array}\right], \left[\begin{array}{r}11\\3\\7\\-5\\\end{array}\right] \right\}$$
+
+## The Column Space
+
+Column Space of $A$
+: The $\text{Span}\{\vec a_1, \vec a_2, \cdots , \vec a_n\}$ where the vectors $\vec a_i$ are the columns of $A$.
+
+So the column space for matrix $R$ from Example 2 has the same basis as the span of the vectors.
+
+## Basis for Null Space of $A$
+
+We have already shown this in section 2.6. We solve the homogeneous system $A\vec x \vec 0$, and write the solution set in vector form.
--- _build/jupyter_execute/ch2/8.m
+++ _build/jupyter_execute/ch2/8.m
@@ -1,3 +1,88 @@
 # 2.8 Bases as Coordinate Systems
 
-This page is under construction
+The standard basis vectors are the columns of the $n\times n$ identity matrices which are generally referred to as $I_n$.
+
+##  Identity matrix and standard basis for $\mathbb R^2$:
+
+I2 = eye(2)
+
+We refer to the standard basis vectors as $\vec e_1$ and $\vec e_2$.
+
+e1 = I2(:,1)
+
+e2 = I2(:,2)
+
+##  Identity matrix and standard basis for $\mathbb R^3$
+
+I3 = eye(3)
+
+Notice that we still use the vectors $\vec e_1$ and $\vec e_2$ even though we are referring to 3-component vectors now.
+
+e1 = I3(:,1)
+
+e2 = I3(:,2)
+
+e3 = I3(:,3)
+
+## Alternate basis $\mathcal B$
+
+Any set of linearly independent vectors $\{\vec v_1,\vec v_2, \dots , \vec v_m \}$ may serve as a basis for the subset it spans. For example, suppose that the vectors $\{\vec v_1,\vec v_2, \vec v_3 \}$ all live in $\mathbb R^3$ and that they are linearly independent. Then, we can use 
+
+$$\mathcal B = \{\vec v_1,\vec v_2, \vec v_3 \}$$
+
+as the basis for $\mathbb R^3$. To see why, let's create a linearly independent set.
+
+### Example: Alternate basis for $\mathbb R^3$
+
+Suppose that we have the following vectors:
+
+$$\vec v_1 =\left[\begin{array}{r}-1\\1\\0\\\end{array}\right],\vec v_2=\left[\begin{array}{r}1\\9\\5\\\end{array}\right],\vec v_3=\left[\begin{array}{r}-1\\-1\\-2\\\end{array}\right] \hspace{1cm}\text{and} \hspace{1cm}\vec w=\left[\begin{array}{r}-3\\5\\2\\\end{array}\right]$$
+
+#### Verifying $\{\vec v_1,\vec v_2, \vec v_3 \}$ is a basis for $\mathbb R^3$
+
+Let's create a matrix whose columns are the candidates for the basis vectors, then row-reduce.
+
+B = [-1 1 -1 ; 1 9 -1 ; 0 5 -2 ]
+
+rref(B)
+
+Because we have 3 pivots, we know the $\text{Span}\{\vec v_1,\vec v_2, \vec v_3 \}$ is a 3-dimensional subspace of $\mathbb R^3$, and the only 3D subspace of $\mathbb R^3$ is $\mathbb R^3$ itself.
+
+#### Finding the $\mathcal B$-coordinates of a point
+
+We want to determine how to write the vector $\vec w$ as a linear combination of the basis vectors. The coordinates $c_1, c_2, c_3$ will be the coefficients of the linear dependence relation when we solve the vector equations
+
+$$c_1\vec v_1+c_2\vec v_2+c_3 \vec v_3=\vec w$$
+
+which is equivalent to the matrix equations
+
+$$B\vec c = \vec w$$
+
+where the matrix $B$ has as its columns the vectors from the basis $\mathcal B$. To do so, we row reduce the augmented matrix.
+
+B = [-1 1 -1 ; 1 9 -1 ; 0 5 -2 ]
+w = [-3 ; 5 ; 2]
+
+We augment the matrix and row reduce $[B,\vec w]$.
+
+A = [ B, w ]
+
+rref(A)
+
+The linear dependence relation tells use the linear combination of the basis vectors that will produce $\vec w$.
+
+$$-4\vec v_1 +(0)\vec v_2 +\vec v_3 = w \hspace{1cm}\implies \hspace{1cm} \vec c = \left[\begin{array}{r}-4\\0\\1\\\end{array}\right]$$
+
+We can verify using the columns of matrix $B$.
+
+b1 = B(:,1)
+b3 = B(:,3)
+-4 * b1 + b3
+
+The $\mathcal B$-coordinates of the vector $\vec w$ are the coefficients of the linear dependence relation. This means we have
+
+$$\vec w_\mathcal B = \left[\begin{array}{r}-4\\0\\1\\\end{array}\right]_\mathcal B$$
+
+where the subscript indicates the corrdinates used to represent the vector. To convert the vector back to standard coordinates, we simply multiply the matrix $B$ by the coordinates from the linear dependence relation.
+
+B * [ 4 ; 0 ; -1]
