c57c54c6cc9761ffe91301bbd879442d6f70d2e0
StraightDraw <robbsinn@gmail.com>
2021-03-01 14:45:39 -0500

    finished lab 5 in section 2.9

--- _build/jupyter_execute/ch2/09/1.m
+++ _build/jupyter_execute/ch2/09/1.m
@@ -12,24 +12,30 @@ If your birthday is in:
 
 $$\vec v_1 =\left[\begin{array}{r}0\\0\\0\\1\\\end{array}\right],\vec v_2=\left[\begin{array}{r}0\\-1\\1\\1\\\end{array}\right],\vec v_3=\left[\begin{array}{r}0\\3\\-3\\4\\\end{array}\right],\vec v_4=\left[\begin{array}{r}-1\\2\\-4\\4\\\end{array}\right]$$
 
+```
 v1 = [0 ; 0 ; 0 ; 1 ];
 v2 = [0 ; -1 ; 1 ; 1 ];
 v3 = [0 ; 3 ; -3 ; 4 ];
 v4 = [-1 ; 2 ; -4 ; 4 ];
+```
 
 $$\vec x_1 =\left[\begin{array}{r}4\\-4\\0\\0\end{array}\right],\vec x_2=\left[\begin{array}{r}1\\-1\\0\\0\end{array}\right],\vec x_3=\left[\begin{array}{r}1\\-5\\0\\0\end{array}\right],\vec x_4=\left[\begin{array}{r}-1\\21\\1\\1\end{array}\right]$$
 
+```
 x1 = [4 ; -4 ; 0 ; 0 ];
 x2 = [1 ; -1 ; 0 ; 0 ];
 x3 = [1 ; -5 ; 0 ; 0 ];
 x4 = [-1 ; 21 ; 1 ; 1 ];
+```
 
 $$\vec y_1 =\left[\begin{array}{r}-4\\0\\0\\-4\\\end{array}\right],\vec y_2=\left[\begin{array}{r}2\\3\\0\\2\\\end{array}\right],\vec y_3=\left[\begin{array}{r}6\\-2\\0\\6\\\end{array}\right],\vec y_4=\left[\begin{array}{r}19\\5\\1\\19\\\end{array}\right]$$
 
+```
 y1 = [-4 ; 0 ; 0 ; -4 ];
 y2 = [2 ; 3 ; 0 ; 2 ];
 y3 = [6 ; -2 ; 0 ; 6 ];
 y4 = [19 ; 5 ; 1 ; 19 ];
+```
 
 ## Part II. Find a basis for the column space of the matrix
 
@@ -43,15 +49,21 @@ If your first name begins with:
 
 $$A = \left[\begin{array}{rrr}1&23&8\\1&18&5\\0&0&1\\\end{array}\right]$$
 
+```
 A = [1 23 8 ; 1 18 5 ; 0 0 1 ];
+```
 
 $$ D = \left[\begin{array}{rrr}1&10&2\\0&0&1\\0&1&-2\\\end{array}\right]$$
 
+```
 D = [1 10 2 ; 0 0 1 ; 0 1 -2 ];
+```
 
 $$J = \left[\begin{array}{rrr}-6&5&-12\\0&1&1\\8&-6&17\\\end{array}\right]$$
 
+```
 J = [-6 5 -12 ; 0 1 1 ; 8 -6 17 ];
+```
 
 ### Part III: Find the $\mathcal B$-coordinates of the vector
 
@@ -69,16 +81,19 @@ If your last name begins with:
 * A pick any of the three.
 
 **Set 1**
+
 $$ \vec b_1 =\left[\begin{array}{r}3\\1\end{array}\right],\vec b_2=\left[\begin{array}{r}4\\1\\\end{array}\right]$$
 
 B = [3 4 ; 1 1 ];
 
 **Set 2**
+
 $$ \vec b_1 =\left[\begin{array}{r}-3\\3\\\end{array}\right],\vec b_2=\left[\begin{array}{r}-7\\2\\\end{array}\right]  $$
 
 B = [-3 -7 ; 3 2 ];
 
 **Set 3**
+
 $$\vec b_1 =\left[\begin{array}{r}-8\\6\\\end{array}\right],\vec b_2=\left[\begin{array}{r}3\\-1\\\end{array}\right]$$
 
 R = [-8 3 ; 6 -1 ];
