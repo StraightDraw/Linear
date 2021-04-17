e16cff6ecc7f2b594e3c38f18b6d6d70bdbb7115
StraightDraw <robbsinn@gmail.com>
2021-04-17 08:06:13 -0400

    beginning ch 5

---
+++ _build/jupyter_execute/ch5/0.m
@@ -0,0 +1,82 @@
+# Chapter 5
+
+## Eigenvalues and Eigenvectors
+
+````{warning}
+Reading these materials and doing the excercises is **not enough** to understand the course material. You must also read and study the corresponding chapters and sections in the Margalit and Rabinoff [*Linear Algebra* online textbook](https://textbooks.math.gatech.edu/ila/chap-eigenvalues.html).
+````
+The information below is like a study guide though much of the information is copied directly from Margalit and Rabinoff. You will be quizzed and tested not only on how to perform various tasks in MATLAB, but on your understanding of the visualizations and discussions in the textbook. Be sure to study these concepts prior to quizzes and tests.
+
+### 5.1 Eigenvalues and Eigenvectors
+
+1. Learn the definition of eigenvector and eigenvalue.
+2. Learn to find eigenvectors and eigenvalues geometrically.
+3. Learn to decide if a number is an eigenvalue of a matrix, and if so, how to find an associated eigenvector.
+4. Key Task: find a basis for the $\lambda$-eigenspace.
+5. Visualization: the geometry of whether or not a vector is an eigenvector.
+
+**Vocabulary words.** Eigenspace, eigenvector, eigenvalue.
+
+### 5.2 Characteristic Polynomial
+
+1. Learn that the eigenvalues of a triangular matrix are the diagonal entries.
+2. Find all eigenvalues of a matrix using the characteristic polynomial.
+3. Learn some strategies for finding the zeros of a polynomial.
+4. Key Task: determine the characteristic polynomial of a $2\times 2$ matrix.
+
+**Vocabulary words.** Characteristic polynomial, trace.
+
+### 5.3 Similarity
+
+1. Learn to interpret similar matrices geoemetrically.
+2. Understand the relationship between the eigenvalues, eigenvectors, and characteristic polynomials of 3. similar matrices.
+4. Key Task: compute $A\vec x$ in terms of $B,C$ for $A = CBC^{−1}$.
+5. Visualization: the geometry of similar matrices.
+
+**Vocabulary word.** Similarity.
+
+### 5.4 Diagonalization
+
+1. Learn two main criteria for a matrix to be diagonalizable.
+2. Develop a library of examples of matrices that are and are not diagonalizable.
+3. Understand what diagonalizability and multiplicity have to say about similarity.
+4. Key Task: diagonalize a matrix.
+5. Visualization: the geometry of diagonal matrices.
+6. The diagonalization theorem (two variants).
+
+**Vocabulary words.** Diagonalizable, algebraic multiplicity, geometric multiplicity.
+
+### 5.5 Complex Eigenvalues
+
+1. Learn to find complex eigenvalues and eigenvectors of a matrix.
+2. Learn to recognize a rotation-scaling matrix, and compute by how much the matrix rotates and scales.
+3. Understand the geometry of $2\times 2$ and $3\times 3$ matrices with a complex eigenvalue.
+4. Understand a $2\times 2$ matrix with a complex eigenvalue is similar to a rotation-scaling matrix.
+5. Key Task: the eigenvector trick for $2\times 2$ matrices.
+6. Visualization: the geometry of matrices with a complex eigenvalue.
+7. The rotation-scaling theorem and the block diagonalization theorem.
+
+**Vocabulary word.** Rotation-scaling matrix.
+
+
+
+
+
+### 5.6 Stochastic Matrices
+
+1. Learn examples of stochastic matrices and applications to difference equations.
+2. Understand Google's PageRank algorithm.
+3. Key Task: find the steady state of a positive stochastic matrix.
+4. Visualization: dynamics of a positive stochastic matrix.
+5. The Perron–Frobenius theorem.
+
+**Vocabulary words.** Difference equation, (positive) stochastic matrix, steady state, importance matrix, Google matrix.
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+1
+```
---
+++ _build/jupyter_execute/ch5/1.m
@@ -0,0 +1,9 @@
+# 5.1 Eigenvalues and Eigenvectors
+
+Eigenvector
+: An eigenvector of $A$ is a nonzero vector $\vec v$ in $\mathbb R^n$ such that $A\vec v = \lambda \vec v$, for some scalar $\lambda$.
+
+Eigenvalue
+: An eigenvalue of $A$ is a scalar $\lambda$ such that the equation $A\vec v = \lambda \vec v$ has a nontrivial solution.
+
+**Terminology.** If $A\vec v = \lambda \vec v$ for some vector $\vec v\neq \vec 0$, we say that $\lambda$ is the eigenvalue for $\vec v$, and that $\vec v$ is an eigenvector for $\lambda$.
